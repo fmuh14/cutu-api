7c1c147a550ee8d2fe9b5b455acabcd15a192cc0
muhammadferdi <ferdimuh12@gmail.com>
2022-12-14 13:21:15 +0700

    Revert "PROJECT SELESAI"
    This reverts commit 1fc7ab68f5f8738f882cabbac731f5e376be2c0b.

--- src/controllers/TransaksiController.ts
+++ src/controllers/TransaksiController.ts
@@ -88,7 +88,7 @@ export class TransaksiController {
         externalID: transaksi.id,
         amount: transaksi.total_harga,
         description: 'Invoice for Transaction #' + transaksi.id,
-        invoice_duration: 300,
+        invoice_duration: 60,
         items: [{
           name: 'Paket ' + paket.name,
           quantity: 1,
